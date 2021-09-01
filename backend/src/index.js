e148bdd248652402ae4af91597e210b9fdf35369
Felipe Trost <felipe.trost@gmail.com>
2021-09-01 06:28:31 +0200

    admin dashboard for socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ module.exports = new RoomGroup();
 const app = require("./app");
 const { Server } = require("socket.io");
 const configSocket = require("./socketio");
+const { instrument } = require("@socket.io/admin-ui");
 const { clientCredentials } = require("./spotify");
 
 const port = process.env.PORT || 5000;
@@ -25,7 +26,15 @@ const getAccessToken = () => accessToken;
 
 const io = new Server(server, {
   cors: {
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL, "https://admin.socket.io"],
+  },
+});
+
+instrument(io, {
+  auth: {
+    type: "basic",
+    username: process.env.SOCKET_USER,
+    password: process.env.SOCKET_PASSWORD,
   },
 });
 
